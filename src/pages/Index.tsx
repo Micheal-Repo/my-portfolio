6a78519ef509f8dcdcd5b4760fdbc14599cd6fee
Micheal-Repo <michealfullstack@gmail.com>
2025-05-08 20:13:01 +0100

    testimonial

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,9 +5,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
-import {
-  Testimonials
-} from "@/components/Testimonials";
+import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
